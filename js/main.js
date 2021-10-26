084fb92973b6ed8dc8fcb45814295826d55e93a9
icuelloarg <icuelloarg@gmail.com>
2021-10-25 22:18:12 -0300

    First deliver curso Javascript

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+
+let nombre = prompt( "Decinos tu nombre");
+let edad = prompt( "Cuál es tu edad?");
+let ciudad = prompt( "En qué ciudad vive usted?");
+let provincia = prompt( "En qué provincia se encuentra?");
+let pais = prompt( "De qué país?");
+let mensaje = "tu nombre es " + nombre + " y tu edad es " + edad + ", vivis en " + 
+ciudad + " de la provincia de " + provincia + " en el país " + pais;
+alert(mensaje)
