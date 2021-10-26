0b682161bf8fb45e63e50a8d8f0a7127b6eebb60
icuelloarg <icuelloarg@gmail.com>
2021-10-26 11:58:03 -0300

    Segunda entrega Javascript

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-
+/* PRIMERA ENTREGA
 let nombre = prompt( "Decinos tu nombre");
 let edad = prompt( "Cuál es tu edad?");
 let ciudad = prompt( "En qué ciudad vive usted?");
@@ -8,3 +8,18 @@ let pais = prompt( "De qué país?");
 let mensaje = "tu nombre es " + nombre + " y tu edad es " + edad + ", vivis en " + 
 ciudad + " de la provincia de " + provincia + " en el país " + pais;
 alert(mensaje)
+*/
+
+// SEGUNDA ENTREGA
+
+let ingresos = Number(parseInt(prompt("Cuál es su ingreso mensual en pesos?")));
+let gastos = Number( parseInt(prompt("Cuánto gasta por mes?")));
+let dinero = Number(ingresos - gastos);
+let aprobado50 = (dinero * 12);
+let aprobado30 = (dinero * 6);
+let aprobado20 = (dinero * 3);
+
+if ((dinero) >= 50000){alert("Podrías solicitar un crédito de " + aprobado50);} 
+else if ((dinero) >= 30000){alert("Podrías solicitar un crédito de " + aprobado30);} 
+else if ((dinero) >= 20000){alert("Podrías solicitar un crédito de " + aprobado20);}
+else {alert("No podemos ofrecerte ningún crédito")}
