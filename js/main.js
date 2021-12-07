8c022e90939c9c53b8c1b7766540390e019998da
icuelloarg <icuelloarg@gmail.com>
2021-12-07 16:05:28 -0300

    Se arreglan errores de commit de entrega

--- js/main.js
+++ js/main.js
@@ -1,24 +1,22 @@
 
-/* Se logró que el programa funcione sumando una funcion reload que permite reiniciar
-las variables para que el programa las tome */
+
+/* Redenominando determinadas variables se logr[o que el programa funcione en todos los navegadores.
+Se utiliza LocalStorage para guardar al solicitante] */
 
 /* Se cambiaron determinadas variables que entraban en conflicto */
 
 
-let nombre = document.getElementById("formName").value;
-let apellido = document.getElementById("formLast").value;
-let dni = document.getElementById("dni").value;
-let correo = document.getElementById("mail").value;
-let telefono = document.getElementById("phone").value;
-let ingresos = Number(document.getElementById("income").value);
-let gastos = Number(document.getElementById("outcome").value);
-const monto = Number(document.getElementById("monto").value);
 let selectTipoCredito = document.getElementById('tipocred');
 let selectCantCuotas = document.getElementById('cuotificador');
 const remover120 = document.getElementById("120c");
 const remover240 = document.getElementById("240c");
 const remover60 = document.getElementById("60c");
-const remover48 = document.getElementById("48c")
+const remover48 = document.getElementById("48c");
+const remover6 = document.getElementById("6c");
+const remover12 = document.getElementById("12c");
+const remover18 = document.getElementById("18c");
+const remover24 = document.getElementById("24c");
+const remover36 = document.getElementById("36c");
 
 
 selectTipoCredito.addEventListener('change', () => {
@@ -36,6 +34,14 @@ selectTipoCredito.addEventListener('change', () => {
             selectCantCuotas.removeChild(remover240);
             break;
         case "3":
+            selectCantCuotas.removeChild(remover6);
+            selectCantCuotas.removeChild(remover12);
+            selectCantCuotas.removeChild(remover18);
+            selectCantCuotas.removeChild(remover24);
+            selectCantCuotas.removeChild(remover36);
+            selectCantCuotas.removeChild(remover60);
+            selectCantCuotas.removeChild(remover48);
+
             break;
         case "4":
             selectCantCuotas.removeChild(remover120);
@@ -47,16 +53,70 @@ selectTipoCredito.addEventListener('change', () => {
 
 });
 
-let localizador = document.getElementById("occupancy");
-localizador.onchange = function localizador1(){
-    location.reload();
+
+
+class Solicitante {
+    constructor (name, last, docu, mail, phone, ammount, object1, pays, incoming, outgoing){
+        this.name = name;
+        this.last = last;
+        this.docu = docu;
+        this.mail = mail;
+        this.phone = phone;
+        this.ammount = ammount;
+        this.object1 = object1;
+        this.pays = pays;
+        this.incoming = incoming;
+        this.outgoing = outgoing;
+
+    }
+    accesoSolicitante() {
+        if (this.nombre != false && this.apellido != false){
+         
+        }
+        else {
+            alert(`Alguno de los datos son incorrectos`);
+        }
+    }
+
 }
 
+const personasD = [];
+
+
+let localizador = document.getElementById("occupancy");
+localizador.onblur = function localizador1(){
+    let nombre1 = document.getElementById("formName").value;
+    let apellido1 = document.getElementById("formLast").value;
+    let dni1 = document.getElementById("dni").value;
+    let correo1 = document.getElementById("mail").value;
+    let telefono1 = document.getElementById("phone").value;
+    let objeto1 = Number(document.getElementById("tipocred").value);
+    let monto1 = Number(document.getElementById("monto").value);
+    let cuotas1 = Number(document.getElementById("cuotificador").value);
+    let ingresos1 = Number(document.getElementById("income").value);
+    let gastos1 = Number(document.getElementById("outcome").value);
+
+
+
+    const solicitador1 = new Solicitante (nombre1, apellido1, dni1, correo1, telefono1, monto1, objeto1, cuotas1, ingresos1, gastos1);
+    personasD.push(solicitador1);
+    const savePersona = JSON.stringify(personasD);
+    localStorage.setItem(`individuo`, savePersona);
+
+
+let nombre = document.getElementById("formName").value;
+let apellido = document.getElementById("formLast").value;
+let dni = document.getElementById("dni").value;
+let correo = document.getElementById("mail").value;
+let telefono = document.getElementById("phone").value;
+let ingresos = Number(document.getElementById("income").value);
+let gastos = Number(document.getElementById("outcome").value);
+let monto = Number(document.getElementById("monto").value);
 let objeto = Number(document.getElementById("tipocred").value);
 let cuotas = Number(document.getElementById("cuotificador").value);
 
 
-let simulador = document.getElementById("submit1");
+let simulador = document.getElementById("submit1");     
 simulador.onclick = function init(){
     simulador.innerHTML = `Solicitar`;
     console.log(nombre);
@@ -75,9 +135,9 @@ function margen () {
     else {return 0};
 }
 
-
-let ingresos = Number(document.getElementById("incomes").value);
-let gastos = Number(document.getElementById("outcomes").value);
+function categoria () {
+    return (margen() * cuotas);
+}
 
 
 class Creditos {
@@ -91,39 +151,10 @@ class Creditos {
 
 const credito = [];
 
-credito.push(new Creditos(1,`Personal`, 0.021, "37"));
-credito.push(new Creditos(2,`Refacciones`, 0.02, "37"));
-credito.push(new Creditos(3,`Hipotecario`, 0.017, "241"));
-credito.push(new Creditos(4,`Prendario`, 0.0192, "61"));
-
-const credJson = JSON.stringify(credito);
-console.log(credJson);
-
-
-// Obtengo ambos select
-
-
-
-
-let simulador = document.getElementById("submit1");
-simulador.onclick = function init(){
-console.log(cuotas)
-    simulador.innerHTML = `Solicitar`;
-    //e.preventDefault();
-
-
-function margen () {
-  
-    if ((ingresos - gastos)>=80000) {return ((ingresos-gastos)*0.8)}
-    else if ((ingresos - gastos)>=50000) {return ((ingresos-gastos)*0.8)}
-    else if ((ingresos - gastos)>=30000) {return ((ingresos-gastos)*0.6)}
-    else if ((ingresos - gastos)>=15000) {return ((ingresos-gastos)*0.5)}
-    else {return 0};
-}
-
-function categoria () {
-    return (margen() * cuotas);
-}
+credito.push(new Creditos(1,`personal`, 0.021, 37));
+credito.push(new Creditos(2,`refacciones`, 0.02, 37));
+credito.push(new Creditos(3,`hipotecario`, 0.017, 241));
+credito.push(new Creditos(4,`prendario`, 0.0192, 61));
 
 const credJson = JSON.stringify(credito);
 console.log(credJson);
@@ -140,39 +171,12 @@ function montoTotal(){
 }
 
 
-
 function calculoCredito () {
     
     return (montoTotal() / cuotas);
 }
 
 
-class Solicitante {
-    constructor (nombre, apellido, dni, correo, telefono){
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.dni = dni;
-        this.correo = correo;
-        this.telefono = telefono;
-    }
-    accesoSolicitante() {
-        if (this.nombre != false && this.apellido != false){
-         
-        }
-        else {
-            alert(`Alguno de los datos son incorrectos`);
-        }
-    }
-
-}
-
-
-
-    const solicitante1 = new Solicitante (nombre, apellido, dni, correo, telefono);
-    solicitante1.accesoSolicitante()
-    console.log(solicitante1)
-
-   
     
 function respuestaCredito (){
     let cuotaPagar = 0;
@@ -180,22 +184,22 @@ function respuestaCredito (){
 
         switch (objeto){
             case 1:
-                if (montoTotal() < categoria()){               
+                if ((cuotas < cuotasMaxima()) && (montoTotal() < categoria())){               
                     cuotaPagar = calculoCredito()}
                 else {alert(`No eres apto para este credito`)};
                 break;
             case 2:
-                if (montoTotal() < categoria()){
+                if ((cuotas < cuotasMaxima()) && (montoTotal() < categoria())){
                     cuotaPagar = calculoCredito()}
                 else {alert(`No eres apto para este credito`)};
                 break;
             case 3:
-                if (montoTotal() < categoria()){
+                if ((cuotas < cuotasMaxima()) && (montoTotal() < categoria())){
                     cuotaPagar = calculoCredito()}
                 else {alert(`No eres apto para este credito`)};
                 break;
             case 4:
-                if (montoTotal() < categoria()){
+                if ((cuotas < cuotasMaxima()) && (montoTotal() < categoria())){
                     cuotaPagar = calculoCredito()}
                 else {alert(`No eres apto para este credito`)};
                 break;
@@ -213,24 +217,23 @@ function respuestaCredito (){
 }
 
 
+
+
+
     respuestaCredito();   
 
     let solicitud = document.getElementById("submit1");
 solicitud.onclick = function valorCuota (){
 
 
-    const solicitante1 = new Solicitante (nombre, apellido, dni, correo, telefono);
-    solicitante1.accesoSolicitante()
-    console.log(solicitante1)
-    const typeSolic = JSON.stringify(solicitante1);
-    console.log(typeSolic);
 
 
     let responderk = document.querySelector(".formulario");
-    let datosP = `Hola ${nombre}, te llamaremos en breve para ofrecerte tu credito de $${monto} al ${telefono}. Gracias!`;
+    let datosP = `Hola ${nombre} ${apellido}, DNI ${dni}, te llamaremos en breve para ofrecerte tu credito de $${monto} al ${telefono}. Gracias!`;
     responderk.innerHTML += `    <div class="divRespuesta">
                                     <textarea class="elemento respCred" name="Text" id="resp2" cols="30" rows="4" disabled>${datosP}</textarea>
                                    
                                 </div>`;}
+
 }
-};
+}
